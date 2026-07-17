b00c7b9075ee4c87adebabdef9023bff8070c708
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-17 15:11:16 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,10 @@
+// Last updated: 17/07/2026, 15:11:16
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4!=0)
+        return true;
+        else
+        return false;
+    }
+};
