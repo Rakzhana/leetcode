89ed247d8407ee5b6294177611c20610b3599c95
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-17 15:12:49 +0530

    Create 111-MinimumDepthOfBinaryTree

---
+++ 111-MinimumDepthOfBinaryTree/111-MinimumDepthOfBinaryTree.py
@@ -0,0 +1,17 @@
+# Last updated: 17/07/2026, 15:12:48
+from collections import deque
+
+class Solution(object):
+    def minDepth(self, root):
+        if not root:
+            return 0
+
+        queue = deque([(root, 1)])
+        while queue:
+            node, depth = queue.popleft()
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.append((node.right, depth + 1))
