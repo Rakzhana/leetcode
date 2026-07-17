eeec86f0e2f4e514c3af4e32aaff78f09ef16456
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-17 15:12:32 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,26 @@
+// Last updated: 17/07/2026, 15:12:31
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char First = s.charAt(start);
+        	char Last = s.charAt(last);
+        	if (!Character.isLetterOrDigit(First )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(Last)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(First) != Character.toLowerCase(Last)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
+        }
+        return true;
+    }
+}
