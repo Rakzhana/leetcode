f8925e47763ec9b8bb76cf95a81902bcbefba18a
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-17 15:11:35 +0530

    Create 222-CountCompleteTreeNodes

---
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.cpp
@@ -0,0 +1,29 @@
+// Last updated: 17/07/2026, 15:11:34
+class Solution {
+   private:
+    int findLeftHeight(TreeNode* node) {
+        int height = 0;
+        while (node) {
+            height++;
+            node = node->left;
+        }
+        return height;
+    }
+    int findRightHeight(TreeNode* node) {
+        int height = 0;
+        while (node) {
+            height++;
+            node = node->right;
+        }
+        return height;
+    }
+
+   public:
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+        int lh = findLeftHeight(root);
+        int rh = findRightHeight(root);
+        if (lh == rh) return (1 << lh) - 1;  // 2^h -1
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
