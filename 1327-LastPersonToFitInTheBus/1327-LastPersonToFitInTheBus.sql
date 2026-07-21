afa54d17c76084b735c72054f3c9fd13385e87dc
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-21 15:54:39 +0530

    Create 1327-LastPersonToFitInTheBus

---
+++ 1327-LastPersonToFitInTheBus/1327-LastPersonToFitInTheBus.sql
@@ -0,0 +1,9 @@
+-- Last updated: 21/07/2026, 15:54:38
+# Write your MySQL query statement below
+SELECT 
+    q1.person_name
+FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <= 1000
+ORDER BY SUM(q2.weight) DESC
+LIMIT 1
