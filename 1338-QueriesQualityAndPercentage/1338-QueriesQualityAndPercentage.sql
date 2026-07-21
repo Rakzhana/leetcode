ceac80d7cbba447546bb1692aeda266adb2743b0
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-21 16:05:31 +0530

    Create 1338-QueriesQualityAndPercentage

---
+++ 1338-QueriesQualityAndPercentage/1338-QueriesQualityAndPercentage.sql
@@ -0,0 +1,10 @@
+-- Last updated: 21/07/2026, 16:05:30
+# Write your MySQL query statement below
+select
+query_name,
+round(avg(cast(rating as decimal) / position), 2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) as poor_query_percentage
+from
+queries
+group by
+query_name;
