f6ff99c2bd2271f675ad13317b7260abcaaf2de9
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-28 09:39:01 +0530

    File updated at 28/07/2026, 09:38:59

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,24 +1,30 @@
-// Last updated: 28/07/2026, 09:38:29
-1public class BSTIterator {
-2    private Stack<TreeNode> stack = new Stack<TreeNode>();
-3    
-4    public BSTIterator(TreeNode root) {
-5        pushAll(root);
-6    }
+// Last updated: 28/07/2026, 09:38:59
+1class Solution {
+2    public int calculateMinimumHP(int[][] dungeon) {
+3        int m = dungeon.length, n = dungeon[0].length;
+4        int[] dp = new int[n + 1];
+5        Arrays.fill(dp, Integer.MAX_VALUE);
+6        dp[n - 1] = 1; // will be computed properly in last row
 7
-8    /** @return whether we have a next smallest number */
-9    public boolean hasNext() {
-10        return !stack.isEmpty();
-11    }
-12
-13    /** @return the next smallest number */
-14    public int next() {
-15        TreeNode tmpNode = stack.pop();
-16        pushAll(tmpNode.right);
-17        return tmpNode.val;
-18    }
-19    
-20    private void pushAll(TreeNode node) {
-21        for (; node != null; stack.push(node), node = node.left);
-22    }
-23}
+8        for (int i = m - 1; i >= 0; i--) {
+9            int[] newDp = new int[n + 1];
+10            Arrays.fill(newDp, Integer.MAX_VALUE);
+11            for (int j = n - 1; j >= 0; j--) {
+12                int bestNext;
+13                if (i == m - 1 && j == n - 1) {
+14                    newDp[j] = Math.max(1, 1 - dungeon[i][j]);
+15                    continue;
+16                }
+17                // right neighbor: newDp[j+1], below neighbor: dp[j]
+18                bestNext = Math.min(
+19                    newDp[j + 1] == Integer.MAX_VALUE ? Integer.MAX_VALUE : newDp[j + 1],
+20                    dp[j] == Integer.MAX_VALUE ? Integer.MAX_VALUE : dp[j]
+21                );
+22                newDp[j] = Math.max(1, bestNext - dungeon[i][j]);
+23            }
+24            dp = newDp;
+25        }
+26
+27        return dp[0];
+28    }
+29}
