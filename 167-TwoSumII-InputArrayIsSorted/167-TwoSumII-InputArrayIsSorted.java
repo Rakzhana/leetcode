bf8ce066a61c63dc01a346944e556b0d8f705cba
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-28 09:37:01 +0530

    File updated at 28/07/2026, 09:36:59

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,20 +1,12 @@
-// Last updated: 28/07/2026, 09:36:17
+// Last updated: 28/07/2026, 09:36:59
 1class Solution {
-2    public int[] twoSum(int[] numbers, int target) {
-3        int left = 0;
-4        int right = numbers.length-1;
-5        while (left < right){
-6            int sum = numbers[right] + numbers[left];
-7            if(sum > target){
-8                right--;
-9            }
-10            else if(sum < target){
-11                left++;
-12            }
-13            else {
-14                return new int[]{left+1,right+1};
-15            }
-16        }
-17        return new int[]{};
-18    }
-19}
+2    public String convertToTitle(int columnNumber) {
+3        StringBuilder sb = new StringBuilder();
+4        while (columnNumber > 0) {
+5            columnNumber--;
+6            sb.append((char) ('A' + columnNumber % 26));
+7            columnNumber /= 26;
+8        }
+9        return sb.reverse().toString();
+10    }
+11}
