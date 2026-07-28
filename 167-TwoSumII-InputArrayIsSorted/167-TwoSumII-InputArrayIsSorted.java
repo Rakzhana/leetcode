a093577d61064ba942845e777eaf4c4d58dbf4f0
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-28 09:39:52 +0530

    File updated at 28/07/2026, 09:39:50

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,30 +1,12 @@
-// Last updated: 28/07/2026, 09:38:59
+// Last updated: 28/07/2026, 09:39:50
 1class Solution {
-2    public int calculateMinimumHP(int[][] dungeon) {
-3        int m = dungeon.length, n = dungeon[0].length;
-4        int[] dp = new int[n + 1];
-5        Arrays.fill(dp, Integer.MAX_VALUE);
-6        dp[n - 1] = 1; // will be computed properly in last row
-7
-8        for (int i = m - 1; i >= 0; i--) {
-9            int[] newDp = new int[n + 1];
-10            Arrays.fill(newDp, Integer.MAX_VALUE);
-11            for (int j = n - 1; j >= 0; j--) {
-12                int bestNext;
-13                if (i == m - 1 && j == n - 1) {
-14                    newDp[j] = Math.max(1, 1 - dungeon[i][j]);
-15                    continue;
-16                }
-17                // right neighbor: newDp[j+1], below neighbor: dp[j]
-18                bestNext = Math.min(
-19                    newDp[j + 1] == Integer.MAX_VALUE ? Integer.MAX_VALUE : newDp[j + 1],
-20                    dp[j] == Integer.MAX_VALUE ? Integer.MAX_VALUE : dp[j]
-21                );
-22                newDp[j] = Math.max(1, bestNext - dungeon[i][j]);
-23            }
-24            dp = newDp;
-25        }
-26
-27        return dp[0];
-28    }
-29}
+2   public List<String> findRepeatedDnaSequences(String s) {
+3    Set seen = new HashSet(), repeated = new HashSet();
+4    for (int i = 0; i + 9 < s.length(); i++) {
+5        String ten = s.substring(i, i + 10);
+6        if (!seen.add(ten))
+7            repeated.add(ten);
+8    }
+9    return new ArrayList(repeated);
+10}
+11}
