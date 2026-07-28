e695a06b185761450d2bc6e378d6c490eccb7aba
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-28 09:37:45 +0530

    File updated at 28/07/2026, 09:37:43

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,12 +1,13 @@
-// Last updated: 28/07/2026, 09:36:59
+// Last updated: 28/07/2026, 09:37:43
 1class Solution {
-2    public String convertToTitle(int columnNumber) {
-3        StringBuilder sb = new StringBuilder();
-4        while (columnNumber > 0) {
-5            columnNumber--;
-6            sb.append((char) ('A' + columnNumber % 26));
-7            columnNumber /= 26;
-8        }
-9        return sb.reverse().toString();
-10    }
-11}
+2    public int trailingZeroes(int n) {
+3        int ans = Math.floorDiv(n, 5);
+4        ans += Math.floorDiv(n, 25);
+5        ans += Math.floorDiv(n, 125);
+6        ans += Math.floorDiv(n, 625);
+7        ans += Math.floorDiv(n, 3125);
+8        
+9        
+10        return ans;
+11    }
+12}
