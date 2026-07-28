3a31b4114b2990a1623ce37b2f0bf0322f1ca57c
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-28 10:28:30 +0530

    File updated at 28/07/2026, 10:28:28

--- 224-BasicCalculator/224-BasicCalculator.java
+++ 224-BasicCalculator/224-BasicCalculator.java
@@ -1,52 +1,34 @@
-// Last updated: 28/07/2026, 10:26:47
-1class Solution {
-2    public int calculate(String s) {
-3        Stack<Integer> st = new Stack<>();
-4
-5        int ans = 0;
-6        int num = 0;
-7        int sign = 1;
-8
-9        for (int i = 0; i < s.length(); i++) {
-10            char ch = s.charAt(i);
-11
-12            if (ch >= '0' && ch <= '9') {
-13                num = num * 10 + (ch - '0');
-14            }
-15
-16            else if (ch == '+') {
-17                ans += sign * num;
-18                num = 0;
-19                sign = 1;
-20            }
-21
-22            else if (ch == '-') {
-23                ans += sign * num;
-24                num = 0;
-25                sign = -1;
-26            }
-27
-28            else if (ch == '(') {
-29                st.push(ans);
-30                st.push(sign);
-31
-32                ans = 0;
-33                sign = 1;
-34            }
-35
-36            else if (ch == ')') {
-37                ans += sign * num;
-38                num = 0;
-39
-40                int prevSign = st.pop();
-41                int prevAns = st.pop();
-42
-43                ans = prevAns + prevSign * ans;
-44            }
-45        }
-46
-47        ans += sign * num;
-48
-49        return ans;
-50    }
-51}
+// Last updated: 28/07/2026, 10:28:28
+1//If this solution helped you consider giving it an upvote
+2class Solution {
+3    public int calculate(String s) {
+4        int res = 0, n = 0;
+5        Stack<Integer> st = new Stack<>();
+6        s += '+';
+7        char op = '+';
+8        for(int i = 0; i < s.length(); i++){
+9            char c = s.charAt(i);
+10
+11            if(c == ' ') continue;
+12
+13            if(Character.isDigit(c)){
+14                n = n * 10 + (c - '0');
+15                continue;
+16            }
+17
+18            if(op == '+') st.push(n);
+19            else if(op == '-') st.push(-n);
+20            else if(op == '*') st.push(st.pop() * n);
+21            else if(op == '/') st.push(st.pop() / n);
+22
+23            op = c;
+24            n = 0;
+25        }
+26
+27        for(int i : st){
+28            res += i;
+29        }
+30
+31        return res;
+32    }
+33}
