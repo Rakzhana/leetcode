45386bc5660855ec2e985a6f7953323970d97dc4
Rakzhana K <rakzhanakumar@gmail.com>
2026-07-28 10:35:15 +0530

    File updated at 28/07/2026, 10:35:13

--- 224-BasicCalculator/224-BasicCalculator.java
+++ 224-BasicCalculator/224-BasicCalculator.java
@@ -1,33 +1,28 @@
-// Last updated: 28/07/2026, 10:29:21
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public List<String> summaryRanges(int[] nums) {
-6        List<String> result = new ArrayList<>();
-7        if (nums.length == 0) {
-8            return result;
+// Last updated: 28/07/2026, 10:35:13
+1class Solution {
+2    public List<Integer> majorityElement(int[] nums) {
+3        // Create a frequency map to store the count of each element
+4        Map<Integer, Integer> elementCountMap = new HashMap<>();
+5        
+6        // Iterate through the input array to count element occurrences
+7        for (int i = 0; i < nums.length; i++) {
+8            elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i], 0) + 1);
 9        }
 10        
-11        int i = 0;
-12        while (i < nums.length) {
-13            int start = nums[i];
-14            int j = i;
-15            // Expand the range as long as elements are consecutive
-16            while (j + 1 < nums.length && nums[j + 1] == nums[j] + 1) {
-17                j++;
-18            }
-19            
-20            // Format the range string
-21            if (nums[j] == start) {
-22                result.add(String.valueOf(start));
-23            } else {
-24                result.add(start + "->" + nums[j]);
-25            }
-26            
-27            // Move to the next potential start of a range
-28            i = j + 1;
-29        }
-30        return result;
-31    }
-32}
+11        List<Integer> majorityElements = new ArrayList<>();
+12        int threshold = nums.length / 3;
+13        
+14        // Iterate through the frequency map to identify majority elements
+15        for (Map.Entry<Integer, Integer> entry : elementCountMap.entrySet()) {
+16            int element = entry.getKey();
+17            int count = entry.getValue();
+18            
+19            // Check if the element count is greater than the threshold
+20            if (count > threshold) {
+21                majorityElements.add(element);
+22            }
+23        }
+24        
+25        return majorityElements;
+26    }
+27}
