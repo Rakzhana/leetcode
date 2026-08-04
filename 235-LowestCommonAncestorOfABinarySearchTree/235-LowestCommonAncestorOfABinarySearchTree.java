70d852ca98edda941ceae670a6ab616b73c6e459
Rakzhana K <rakzhanakumar@gmail.com>
2026-08-04 10:20:38 +0530

    Create 235-LowestCommonAncestorOfABinarySearchTree

---
+++ 235-LowestCommonAncestorOfABinarySearchTree/235-LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,16 @@
+// Last updated: 04/08/2026, 10:20:35
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int small = Math.min(p.val, q.val);
+        int large = Math.max(p.val, q.val);
+        while (root != null) {
+            if (root.val > large) // p, q belong to the left subtree
+                root = root.left;
+            else if (root.val < small) // p, q belong to the right subtree
+                root = root.right;
+            else // Now, small <= root.val <= large -> This root is the LCA between p and q
+                return root;
+        }
+        return null;
+    }
+}
