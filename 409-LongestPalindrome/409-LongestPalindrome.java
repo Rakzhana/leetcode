3a5bcf7fc9a9ae5727dedf6480f6b5c24a329e8c
Rakzhana K <rakzhanakumar@gmail.com>
2026-08-27 09:41:58 +0530

    File updated at 27/08/2026, 09:41:54

--- 409-LongestPalindrome/409-LongestPalindrome.java
+++ 409-LongestPalindrome/409-LongestPalindrome.java
@@ -1,21 +1,17 @@
-// Last updated: 27/08/2026, 09:27:16
+// Last updated: 27/08/2026, 09:41:54
 1class Solution {
-2    public String addStrings(String num1, String num2) {
-3        int i= num1.length()-1;
-4        int j = num2.length()-1;
-5        int carry =0;
-6        StringBuilder sb = new StringBuilder();
-7        while(i>=0||j>=0||carry!=0)
-8        {
-9            int d1=(i>=0)?num1.charAt(i)-'0':0;
-10            int d2=(j>=0)?num2.charAt(j)-'0':0;
-11            int sum = d1+d2+carry;
-12             carry = sum/10;
-13            sb.append(sum%10);
-14            i--;
-15            j--;
-16        }
-17        return sb.reverse().toString();
-18        
-19    }
-20}
+2    public int countSegments(String s) {
+3        // Split the string into words using spaces as separators
+4        String[] words = s.split(" ");
+5        int count = 0;
+6
+7        // Count non-empty words
+8        for (String word : words) {
+9            if (!word.isEmpty()) {
+10                count++;
+11            }
+12        }
+13
+14        return count;
+15    }
+16}
