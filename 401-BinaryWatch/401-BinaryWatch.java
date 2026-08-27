690ac85a866c4ae0c938ab5d3f3203399c2b8fc1
Rakzhana K <rakzhanakumar@gmail.com>
2026-08-27 09:29:25 +0530

    Create 401-BinaryWatch

---
+++ 401-BinaryWatch/401-BinaryWatch.java
@@ -0,0 +1,24 @@
+// Last updated: 27/08/2026, 09:29:21
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+        
+        for (int hour = 0; hour < 12; hour++) {
+            
+            for (int minute = 0; minute < 60; minute++) {
+                
+                int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
+                
+                if (totalBits == turnedOn) {
+                    
+                    String time = hour + ":" + 
+                                  (minute < 10 ? "0" + minute : minute);
+                    
+                    result.add(time);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
