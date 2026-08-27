52a3c0131c480b7f1034049e20e35a3e3779b0eb
Rakzhana K <rakzhanakumar@gmail.com>
2026-08-27 09:27:20 +0530

    File updated at 27/08/2026, 09:27:16

--- 409-LongestPalindrome/409-LongestPalindrome.java
+++ 409-LongestPalindrome/409-LongestPalindrome.java
@@ -1,30 +1,21 @@
-// Last updated: 27/08/2026, 09:25:43
+// Last updated: 27/08/2026, 09:27:16
 1class Solution {
-2    public int thirdMax(int[] nums) {
-3       long max1 = Long.MIN_VALUE;
-4        long max2 = Long.MIN_VALUE;
-5        long max3 = Long.MIN_VALUE;
-6
-7        for (int num : nums) {
-8           
-9            if (num == max1 || num == max2 || num == max3) {
-10                continue;
-11            }
-12
-13            if (num > max1) {
-14                max3 = max2;
-15                max2 = max1;
-16                max1 = num;
-17            } else if (num > max2) {
-18                max3 = max2;
-19                max2 = num;
-20            } else if (num > max3) {
-21                max3 = num;
-22            }
-23        }
-24
-25        // If third max doesn't exist, return the largest
-26        return (max3 == Long.MIN_VALUE) ? (int) max1 : (int) max3;
-27
-28    }
-29}
+2    public String addStrings(String num1, String num2) {
+3        int i= num1.length()-1;
+4        int j = num2.length()-1;
+5        int carry =0;
+6        StringBuilder sb = new StringBuilder();
+7        while(i>=0||j>=0||carry!=0)
+8        {
+9            int d1=(i>=0)?num1.charAt(i)-'0':0;
+10            int d2=(j>=0)?num2.charAt(j)-'0':0;
+11            int sum = d1+d2+carry;
+12             carry = sum/10;
+13            sb.append(sum%10);
+14            i--;
+15            j--;
+16        }
+17        return sb.reverse().toString();
+18        
+19    }
+20}
