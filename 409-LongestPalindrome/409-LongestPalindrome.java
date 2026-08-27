87129f6af237e5870b813e044f4ca1da2cecad5b
Rakzhana K <rakzhanakumar@gmail.com>
2026-08-27 09:43:17 +0530

    File updated at 27/08/2026, 09:43:14

--- 409-LongestPalindrome/409-LongestPalindrome.java
+++ 409-LongestPalindrome/409-LongestPalindrome.java
@@ -1,17 +1,21 @@
-// Last updated: 27/08/2026, 09:41:54
+// Last updated: 27/08/2026, 09:43:14
 1class Solution {
-2    public int countSegments(String s) {
-3        // Split the string into words using spaces as separators
-4        String[] words = s.split(" ");
-5        int count = 0;
-6
-7        // Count non-empty words
-8        for (String word : words) {
-9            if (!word.isEmpty()) {
-10                count++;
+2    public int arrangeCoins(int n) {
+3        int low=1;
+4        int high=n;
+5        int ans=1;
+6        while(low<=high){
+7            int mid=low+(high-low)/2;
+8            if(helper(mid)<=n){
+9                ans=mid;
+10                low=mid+1;
 11            }
-12        }
-13
-14        return count;
+12            else high=mid-1;
+13        }
+14        return ans;
 15    }
-16}
+16    public long helper(int mid){
+17        long sum=(long)mid*(mid+1)/2;
+18        return sum;
+19    }
+20}
